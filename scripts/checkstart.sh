553c5ad6ddefa5af00267ff8989f0436eb8bcc55
Anton <af001@users.noreply.github.com>
2017-03-14 21:56:55 -0400

    added pi-timolo

--- scripts/checkstart.sh
+++ scripts/checkstart.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
 ## checkstart.sh
-# Determine if the script is running.
+# Determine if the scripts are running.
 # By Anton Foltz
 
 result=`ps aux | grep -i "watch.py" | grep -v "grep" | wc -l`
@@ -10,6 +10,14 @@ if [ $result -ge 1 ]
         exit 1
    else
         /home/pi/pi-detector/watch.sh start > /dev/null &
+fi
+
+result=`ps aux | grep -i "pi-timolo.py" | grep -v "grep" | wc -l`
+if [ $result -ge 1 ]
+   then
+        exit 1
+   else
+        /home/pi/pi-timolo/pi-timolo.sh start > /dev/null &
 fi
 
 exit 0
