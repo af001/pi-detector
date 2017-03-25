568e3ad73ee473da4ef0b25e0fb759f6808a762c
Anton <af001@users.noreply.github.com>
2017-03-25 15:20:09 -0400

    Updates to clear EOF error

--- scripts/install.sh
+++ scripts/install.sh
@@ -23,7 +23,9 @@ cd
 echo '[+] Installing Pi motion sensor module...'
 curl -L https://raw.github.com/pageauc/pi-timolo/master/source/pi-timolo-install.sh | bash
 mv $HOME/pi-timolo /home/pi/pi-timolo
+mv $HOME/pi-detector /home/pi/pi-detector
 chown -R pi:pi /home/pi/pi-timolo
+chown -R pi:pi /home/pi/pi-detector
 
 echo '[+] Adding auto launch for pi-timolo to start on boot'
 sed -i "\$i su pi -c \"/home/pi/pi-timolo/pi-timolo.sh start > /dev/null &\"" /etc/rc.local
@@ -32,11 +34,12 @@ sed -i "\$i su pi -c \"/home/pi/pi-detector/watch.sh start > /dev/null &\"" /etc
 
 echo '[+] Adding cronjob and changing file permissions...'
 chmod -R +x /home/pi/pi-detector
-chown -R pi:pi /home/pi/pi-detector
 crontab -l > picron
 echo "*/5 * * * * /home/pi/pi-detector/scripts/checkstart.sh" >> picron
 crontab picron
 rm picron
+
+echo '[+] Done!'
 
 exit 0
 
