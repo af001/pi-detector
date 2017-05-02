27982b1d7901c257055e41639f2f9036d80a4f70
geoffroy <geogeob@hotmail.com>
2017-05-02 18:42:38 -0400

    Exception - continued 2

--- watch.py
+++ watch.py
@@ -16,20 +16,20 @@ class MotionEventHandler(PatternMatchingEventHandler):
 
     def on_created(self, event):
         print(event.src_path, event.event_type)
-        file = open(event.src_path, 'rb')
-        image = file.read()
+        event_file = open(event.src_path, 'rb')
+        image = event_file.read()
 
         response_check = client.detect_faces(Image={'Bytes': image})
         if not response_check['FaceDetails']:
-            print('No face detected, deleting file...')
-            file.flush()
-            file.close()
+            print 'No face detected, deleting file...'
+            event_file.flush()
+            event_file.close()
             try:
                 os.remove(event.src_path)
             except OSError:
-                print('Cannot delete file, check permissions')
+                print 'Cannot delete file, check permissions'
         else:
-            print('Face detected, identifying...')
+            print 'Face detected, identifying...'
             try:
                 resp = client.search_faces_by_image(
                     CollectionId=collection,
@@ -45,18 +45,19 @@ class MotionEventHandler(PatternMatchingEventHandler):
                             time.strftime('%Y-%m-%d %H:%M:%S'),
                             resp['FaceMatches'][0]['Face']['ExternalImageId'],
                             event.src_path))
-            except ClientError as e:
-                print('Client error: %s' % e.response['Error']['Code'])
-            file.flush()
-            file.close()
+            except ClientError as exception:
+                print 'Error: %s' % exception.response['Error']['Code']
+            event_file.flush()
+            event_file.close()
 
 def main():
     path = sys.argv[1] if len(sys.argv) > 1 else '.'
     collection = sys.argv[2] if len(sys.argv) > 2 else 'camerapi'
+    print collection
     observer = Observer()
     event_handler = MotionEventHandler()
     observer.schedule(event_handler, path)
-    print('Starting observer on %s' % path)
+    print 'Starting observer on %s' % path
     observer.start()
 
     try:
